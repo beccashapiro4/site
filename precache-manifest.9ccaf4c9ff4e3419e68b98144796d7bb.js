3a3f29d618ae3e6c5b0848b43c7924f32de40401
Becca Shapiro <beccashapiro4@gmail.com>
2022-07-03 00:51:14 -0400

    work-please

---
+++ precache-manifest.9ccaf4c9ff4e3419e68b98144796d7bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2841dd9e4ed46f5cd09",
+    "url": "/static/js/main.e2841dd9.chunk.js"
+  },
+  {
+    "revision": "4799bedcdb985cefbc8a",
+    "url": "/static/js/1.4799bedc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd7de983ac7fc00da6d6027b2a3687d9",
+    "url": "/index.html"
+  }
+];
