bf8d4a20cb50ede3167aa1733033e385746fa1a0
Becca Shapiro <beccashapiro4@gmail.com>
2022-07-11 21:18:51 -0400

    fix links

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -11,12 +11,15 @@ const About = () => {
               <img className='left-side' id='rainbow-logo' src={'assets/img/fish/fish-hand.PNG'} />
               <div className="right-side" >
                 <h2> Nice To Meet You </h2>
+                <h5>...this site is a work in progress...</h5>
                 <p>
-                  Becca Shapiro is an author/illustrator/filmmaker living in Queens, New York.
-                  Becca grew up in Lower Merion, PA. In 2019 Becca received her B.A. in Mathematics and Creative Writing
-                  from Duke University. Becca helped found the experimental social media startup <a href='https://www.muze.nyc'> Muze </a>
-                  where she worked until May 2021. In summer 2021 Becca attended the Prague Writer's Workshop.
-                  In addition to her creative pursuits, Becca works as a software engineer and a test prep tutor.
+                  Becca Shapiro is an author/illustrator who splits time between Philadelphia, PA and Queens, NY.
+                  In 2019 Becca received her B.A. in Mathematics and Creative Writing
+                  from Duke University. She went on to create the experimental social media startup
+                  <a href="https://techcrunch.com/2020/08/25/muze-redesigns-mobile-messaging-as-a-free-form-canvas-for-creativity/"> Muze Collective </a>
+                  where she worked until May 2021. In addition to her creative pursuits, Becca works as a software engineer, tutor,
+                  and anything else you'll pay her for within reasonable physical and/or legal safety standards.
+                  Please reach out via email or Instagram with any questions or if you just want to chat.
                 </p>
                 <a href='https://en.wikipedia.org/wiki/Special:Random'><img src={'assets/img/fish/work-mode.jpg'} style={{ width: '10%' }} /></a>
               </div>
