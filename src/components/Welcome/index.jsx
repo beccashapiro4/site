b4e40a0ad9997a9537b6120186c67b2789418f72
Becca Shapiro <beccashapiro4@gmail.com>
2023-03-08 23:52:45 -0500

    Fix unclickable navbar on home page

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -7,21 +7,17 @@ import "firebase/compat/auth"
 import "firebase/compat/firestore"
 import Navbar from '../Navbar';
 
-//style={{ backgroundImage: 'url(/assets/img/fish/dog-cat.jpg)', backgroundSize: 'cover', height: '1000px' }}
-
 const Welcome = () => {
   const firebaseApp = firebase.apps[0]
   return (
     <div>
       <main className="main-content bg-gray">
-
         <div className="row" style={{ backgroundImage: 'url(/assets/img/fish/dog-cat.jpg)', backgroundSize: 'cover', height: '1000px', backgroundPosition: 'center' }}>
-          <div className="col-12 col-lg-6 offset-lg-3">
-            <div style={{ position: 'relative', zIndex: 100 }} >
-              < ReactFloatingBalloons />
-            </div>
+          <div style={{ position: 'relative' }} >
+            < ReactFloatingBalloons />
           </div>
         </div>
+
       </main>
 
     </div>
