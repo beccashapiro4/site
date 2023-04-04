efbaf8ea42459d332b94f8b335bd90e176ed1d18
Becca Shapiro <beccashapiro4@gmail.com>
2023-04-04 18:40:08 -0400

    update about copy

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,23 +10,26 @@ const About = () => {
             <div className="row" id="about">
               <img className='left-side' id='rainbow-logo' src={'assets/img/fish/fish-hand.PNG'} />
               <div className="right-side" >
-                <h2> About Becca Shapiro </h2>
-                <h5>I made this website.</h5>
-                <h6>(Don't click too aggressively, it will break.)</h6>
+                <h2> Howdy! </h2>
+                <h5>Sit down, stay awhile. Coat rack is to the left.</h5>
                 <p>
-                  Howdy! My name's Becca. Some of my friends call me "Dragon Lord" but you don't have to. Just a suggestion.
+                  My name's Becca Shapiro.
                   I'm an author/visual artist/amateur comedian in Sunnyside, Queens.
                   I spend most of my time reading or pacing.
-                  I own a small but respectable collection of plastic lobsters. They are not real, and therefore will never die.
-                  Born and raised in Philly, I support the Philadelphia Eagles despite my general distaste for traumatic brain injuries.
-                  I studied Math & Creative Writing at Duke University. I say "y'all" now.
-                  After graduating I moved to New York to build the experimental social media startup
+                  I own a small but respectable collection of plastic lobsters.
+                  I own no real lobsters, nor am I a certified scuba diver.
+                  Wonderful news: the shape of the to-go coffee cup works to the advantage of every member of the to-go coffee supply chain (manufactures, suppliers, consumers),
+                  despite its unintuitive top-heavy design.
+                  Born and raised in Philly, I support the Philadelphia Eagles. I struggle to negotiate my football fanaticism with my general distaste for traumatic brain injuries.
+                  I studied Math & Creative Writing at Duke University.
+                  I consistently finished last in both Slalom and Giant Slalom USCSA ski races.
+                  I moved to New York to build the experimental social media startup
                   <a href="https://techcrunch.com/2020/08/25/muze-redesigns-mobile-messaging-as-a-free-form-canvas-for-creativity/"> Muze Collective </a>
-                  with a few of my buddies. I coded this website; it only halfway functions.
-                  I’m currently a barista at The Center For Fiction in Brooklyn, NY where I get to say funny stuff and babble about books.
-                  I've got a bunch of projects in the works, including an animated show called 'Freshwater' about anthropomorphic fish,
-                  a collection of oil paintings called 'Fish & Phalanges,' and a prog-rock-EDM-noise-punk band that convenes annually on the first full moon of August.
-                  In addition to my creative pursuits, I work as an SAT/ACT tutor, freelance writer, animator,
+                  with a few of my buddies. I spent some time in Prague, writing and reading (and pacing).
+                  I’m currently a barista/bartender at The Center For Fiction in Brooklyn, NY.
+                  I've got a few projects in the works, primarily an animated show called 'Freshwater,' which I am scripting, animating, and recording.
+                  Other works-in-progress include: a collection of oil paintings called 'Fish & Phalanges' and a prog-rock-EDM-noise-punk band that convenes annually on the first full moon of August.
+                  In addition to my creative pursuits, I work as an SAT/ACT tutor, web developer/designer, freelance writer, animator,
                   and just about anything else you'll pay for within reasonable physical and/or legal limits.
                   Please reach out via email or Instagram with any questions or if you wanna say what's up :)
                 </p>
@@ -34,7 +37,7 @@ const About = () => {
                   - B
                 </p>
                 <p>
-                  P.S. If you haven't tried popping the bubbles on my home screen, you really should.
+                  P.S. Apologies for technical difficulties! I am still in the process of developing this site.
                 </p>
                 <a href='https://en.wikipedia.org/wiki/Special:Random'><img src={'assets/img/fish/work-mode.jpg'} style={{ width: '10%' }} /></a>
               </div>
