bba1df6d6d8ab75dd94e5be64b46dbca9a7b6233
Becca Shapiro <beccashapiro4@gmail.com>
2024-03-01 01:02:53 -0500

    add portfolio
    although it's not working

---
+++ src/components/Portfolio/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <main>
+                <div style={{
+                    backgroundImage: 'url(../../../assets/img/fish/Portfolio.png)', backgroundSize: 'cover'
+                }}>
+                    <h2>Hello there</h2>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                </div>
+            </main>
+
+        </div>
+    );
+};
+
+export default Portfolio;
