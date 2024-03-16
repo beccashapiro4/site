67ca6ff103ff815cb535ae4cc77af30f2fd3a750
Becca Shapiro <beccashapiro4@gmail.com>
2024-03-16 16:15:45 -0400

    Updated my bio

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,27 +10,23 @@ const About = () => {
             <div className="row" id="about">
               <img className='left-side' id='rainbow-logo' src={'assets/img/fish/fish-hand.PNG'} />
               <div className="right-side" >
-                <h2> Howdy! </h2>
-                <h5>Sit down, stay awhile. Coat rack is to the left.</h5>
+                <h2> You look great today. </h2>
+                <h5>Seriously. Love what you're doing with your hair.</h5>
                 <p>
-                  My name's Becca Shapiro.
-                  I'm an author/visual artist/amateur comedian in Sunnyside, Queens.
-                  I spend most of my time reading or pacing.
-                  I own a small but respectable collection of plastic lobsters.
-                  I own no real lobsters, nor am I a certified scuba diver.
-                  Wonderful news: the shape of the to-go coffee cup works to the advantage of every member of the to-go coffee supply chain (manufactures, suppliers, consumers),
-                  despite its unintuitive top-heavy design.
-                  Born and raised in Philly, I support the Philadelphia Eagles. I struggle to negotiate my football fanaticism with my general distaste for traumatic brain injuries.
-                  I studied Math & Creative Writing at Duke University.
-                  I consistently finished last in both Slalom and Giant Slalom USCSA ski races.
-                  I moved to New York to build the experimental social media startup
-                  <a href="https://techcrunch.com/2020/08/25/muze-redesigns-mobile-messaging-as-a-free-form-canvas-for-creativity/"> Muze Collective </a>
-                  with a few of my buddies. I spent some time in Prague, writing and reading (and pacing).
-                  I’m currently a barista/bartender at The Center For Fiction in Brooklyn, NY.
-                  I've got a few projects in the works, primarily an animated show called 'Freshwater,' which I am scripting, animating, and recording.
-                  Other works-in-progress include: a collection of oil paintings called 'Fish & Phalanges' and a prog-rock-EDM-noise-punk band that convenes annually on the first full moon of August.
-                  In addition to my creative pursuits, I work as an SAT/ACT tutor, web developer/designer, freelance writer, animator,
-                  and just about anything else you'll pay for within reasonable physical and/or legal limits.
+                  My name’s Becca Shapiro. I’m a multidisciplinary artist, working across eclectic mediums including: fiction writing, screenwriting, illustration, animation, & standup comedy.
+                  I was born in Philly, now based in Queens, NY.
+                  I graduated from Duke University with a B.A. in Mathematics and Creative Writing.
+                  I’ve nearly always got a pen in my hand and pins & needles in my toes.
+                </p>
+                <p>
+                  My main pursuit at the moment is the creation (writing, directing, animating, scoring) of the animated show FRESHWATER: an absurdist dark-comedy about anthropomorphic fish struggling with existential dread.
+                  I'm also developing a live-action feature, and completing a series of oil paintings called "Fish & Phalanges."
+                </p>
+                <p>
+                  When I'm not neck-deep in fictional worlds, you can find me behind the bar at The Center For Fiction in Fort Greene, Brooklyn, where I work as a barista/bartender and enthusiastic book-recommender.
+                </p>
+                <p style={{ fontWeight: 'bold' }}>
+                  I’m currently open for writing assignments, design work and commissions.
                   Please reach out via email or Instagram with any questions or if you wanna say what's up :)
                 </p>
                 <p>
