2f8bb55cb6c39ce02f4a4d0ff5ade2919a623add
Becca Shapiro <beccashapiro4@gmail.com>
2024-03-16 15:57:14 -0400

    fix bubbles (downgrade styled-components)

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -12,7 +12,7 @@ const Welcome = () => {
   return (
     <div>
       <main className="main-content bg-gray">
-        <div className="row" style={{ backgroundImage: 'url(/assets/img/fish/dog-cat.jpg)', backgroundSize: 'cover', height: '1000px', backgroundPosition: 'center' }}>
+        <div className="row" style={{ backgroundImage: 'url(/assets/img/fish/dog-cat.jpg)', backgroundSize: 'cover', height: '100vh', backgroundPosition: 'center' }}>
           <div style={{ position: 'relative' }} >
             < ReactFloatingBalloons />
           </div>
